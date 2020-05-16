eb71fb083e559cc44a67551f57bc46e319c5b8d3
luckyLCD <1763163252@qq.com>
2020-05-16 12:16:11 +0800

    第二种方式，登录业务功能完成

--- admin/js/login.js
+++ admin/js/login.js
@@ -1,26 +1,31 @@
+// 入口函数
 $(function () {
-    $('.input_sub').on('click', function (e) {
+    $('.login_form').on('submit', function (e) {
         // 阻止默认提交行为
         e.preventDefault();
-        // 判断用户名和密码是否为空
-        var username = $('.input_txt').val();
-        var password = $('.input_pass').val();
-        if ($.trim(username) == '' || $.trim(password) == '') {
-            alert('用户名或密码不能为空，请重新输入');
-            return;
-        }
         // 发送ajax请求
         $.ajax({
             type: 'post',
             url: 'http://localhost:8080/api/v1/admin/user/login',
-            data: {
-                username: username,
-                password: password
+            data: $('.login_form').serialize(),
+            // 发送请求前，判断用户名或密码是否为空
+            beforeSend: function () {
+                var $obj = $('.login_form input[name]')
+                var flag = false;
+                $obj.each(function () {
+                    if ($.trim($(this).val()) == '') {
+                        flag = true;
+                    }
+                })
+                if (flag) {
+                    alert('用户名或密码为空，请重新输入');
+                    return false;
+                }
             },
             success: function (res) {
                 // console.log(res);
                 if (res.code == 200) {
-                    alert(res.msg);
+                    alert('登录成功');
                     window.location.href = './index.html';
                 } else {
                     alert(res.msg);
