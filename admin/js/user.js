131e231190dd63fc3a1991824fd5784249b01796
luckyLCD <1763163252@qq.com>
2020-05-16 21:50:18 +0800

    个人信息数据获取业务功能完成

--- admin/js/user.js
+++ admin/js/user.js
@@ -1,14 +1,21 @@
 // 页面一加载，ajax请求个人详细信息，渲染页面
-/* $(function () {
+$(function () {
     $.ajax({
         type: 'get',
         url: BigNew.user_detail,
         // dataType: 'json',
-        header: {
+        headers: {
             'Authorization': localStorage.getItem('token')
         },
         success: function (res) {
             console.log(res);
+            if (res.code == 200) {
+                $('input.username').val(res.data.username);
+                $('input.nickname').val(res.data.nickname);
+                $('input.email').val(res.data.email);
+                $('.user_pic').attr('src', res.data.userPic);
+                $('input.password').val(res.data.password);
+            }
         }
     })
-}) */
+})
