c7d7eb1a00ef6239e1a5900d2e13345e81a70f4f
luckyLCD <1763163252@qq.com>
2020-05-17 22:19:44 +0800

    删除文章分类业务功能完成

--- admin/js/article_category.js
+++ admin/js/article_category.js
@@ -5,7 +5,7 @@ $(function () {
             type: 'get',
             url: BigNew.category_list,
             success: function (res) {
-                console.log(res);
+                // console.log(res);
                 window.info = res.data;
                 var htmlStr = template('modal', { list: res.data });
                 $('.category_table tbody').html(htmlStr);
@@ -15,11 +15,11 @@ $(function () {
 
     // 新增分类
     $('#xinzengfenlei').on('click', function () {
-        $('.modal').modal('show');
+        $('.addModal').modal('show');
         $('.modal-title').text('新增分类');
         $('#form')[0].reset();
     })
-    $('.modal .confirm').on('click', function () {
+    $('.addModal .confirm').on('click', function () {
         var id = $('input[name="id"]').val();
         $.ajax({
             type: 'post',
@@ -30,10 +30,10 @@ $(function () {
                 slug: $('input[name="slug"]').val()
             }, */
             success: function (res) {
-                console.log(res);
-                $('.modal').modal('hide');
+                // console.log(res);
+                $('.addModal').modal('hide');
                 if (res.code == 201 || res.code == 200) {
-                    $('.modal').on('hidden.bs.modal', function (e) {
+                    $('.addModal').on('hidden.bs.modal', function (e) {
                         render();
                     })
                 }
@@ -46,18 +46,41 @@ $(function () {
 
     // 编辑分类
     $('.category_table').on('click', '.btn-info', function () {
-        $('.modal').modal('show');
+        $('.addModal').modal('show');
         $('.modal-title').text('编辑分类');
 
         var idx = $(this).data('index');
         // console.log(idx);
         // console.log(info);
         var obj = info[idx];
-        window.id = obj.id;
+        // window.id = obj.id;
 
         $('input[name="id"]').val(obj.id);
         $('input[name="name"]').val(obj.name);
         $('input[name="slug"]').val(obj.slug);
     })
 
+    // 删除分类
+    $('.category_table').on('click', '.btn-danger', function () {
+        $('.delModal').modal('show');
+        window.id = $(this).data('id');
+    })
+    $('.btn-sure-delete').on('click', function () {
+        $.ajax({
+            type: 'post',
+            url: BigNew.category_delete,
+            data: {
+                id: id
+            },
+            success: function (res) {
+                // console.log(res);
+                $('.delModal').modal('hide');
+                if (res.code == 204) {
+                    $('.delModal').on('hidden.bs.modal', function (e) {
+                        render();
+                    })
+                }
+            }
+        })
+    })
 })
