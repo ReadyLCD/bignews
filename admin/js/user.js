893d46fd226c3b920aaf0db13ed957f6002af5e3
luckyLCD <1763163252@qq.com>
2020-05-17 17:03:47 +0800

    上传图片并预览业务功能完成

--- admin/js/user.js
+++ admin/js/user.js
@@ -8,7 +8,7 @@ $(function () {
             'Authorization': localStorage.getItem('token')
         },
         success: function (res) {
-            console.log(res);
+            // console.log(res);
             if (res.code == 200) {
                 $('input.username').val(res.data.username);
                 $('input.nickname').val(res.data.nickname);
@@ -18,4 +18,31 @@ $(function () {
             }
         }
     })
+
+    // 上传img图片并预览
+    $('#exampleInputFile').on('change', function () {
+        // console.dir(this);
+        // 通过URL.createObjectURL生成一个临时的可访问图片地址
+        var file = URL.createObjectURL(this.files[0]);
+        $('.user_pic').attr('src', file);
+    })
+
+    /* // 修改数据提交
+    $('#form').on('submit', function (e) {
+        // 阻止默认提交行为
+        e.preventDefault();
+        // 发送ajax请求
+        // FormData对象对象收集的数据转换成二进制
+        var data = new FormData(this);
+        $.ajax({
+            type: 'post',
+            url: BigNew.user_edit,
+            data: data,
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                console.log(res);
+            }
+        })
+    }) */
 })
