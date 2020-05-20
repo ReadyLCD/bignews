076fd411ede9b224bd5574cea6f6f1f11a223328
luckyLCD <1763163252@qq.com>
2020-05-20 17:38:17 +0800

    文章列表中删除文章业务功能和解决删除最后一页所有数据分页控件的bug，其实算是优化吧

--- admin/js/article_list.js
+++ admin/js/article_list.js
@@ -32,8 +32,16 @@ $(function () {
                     $('#pagination-demo').hide().next().show();
                 } else if (res.data.totalPage != 0 && callback != null) {
                     $('#pagination-demo').show().next().hide();
-
                     callback(res);
+                } else if (res.data.totalPage != 0 && res.data.data.length == 0) {
+                    // 当前页的数据被删除完，要页码要减1，主要针对最后一页
+                    currentPage -= 1;
+                    // 重绘控件页码
+                    // 更新分页控件的总页码 
+                    // 1. 第1个参数是一个事件 当页码值发生变化时就会触发
+                    // 2. 第2个参数是 要变化的新的总页码值
+                    // 3. 第3个参数 是当前页码值
+                    $('#pagination-demo').twbsPagination('changeTotalPages', res.data.totalPage, currentPage);
                 }
             }
         })
@@ -43,12 +51,14 @@ $(function () {
     $('#btnSearch').on('click', function (e) {
         // 阻止默认提交行为
         e.preventDefault();
+        // 重绘控件页码
         getArticleDataList(1, function (res) {
             $('#pagination-demo').twbsPagination('changeTotalPages', res.data.totalPage, 1);
         })
     })
 
     // 实现分页功能
+    var currentPage = 1;
     function pagination(res, visiblePages) {
         $('#pagination-demo').twbsPagination({
             totalPages: res.data.totalPage,  //总页数
@@ -59,8 +69,32 @@ $(function () {
             prev: '上一页',
             initiateStartPageClick: false,  //插件初始化时在起始页面上点击 false
             onPageClick: function (event, page) {
+                currentPage = page;
                 getArticleDataList(page, null);
             }
         });
     }
+
+    // 删除文章列表中文章
+    var articleId;
+    $('#delModal').on('show.bs.modal', function (e) {
+        console.log(e.relatedTarget);
+        articleId = $(e.relatedTarget).data('id');
+    })
+    $('.delModal .btn-sure').on('click', function () {
+        $.ajax({
+            type: 'post',
+            url: BigNew.article_delete,
+            data: {
+                id: articleId
+            },
+            success: function (res) {
+                // console.log(res);
+                if (res.code == 204) {
+                    $('.modal').modal('hide');
+                    getArticleDataList(currentPage, null);
+                }
+            }
+        })
+    })
 })
