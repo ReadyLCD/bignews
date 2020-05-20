d1bbd40fa4baa18c88ccaef955706ad51cf2ae91
luckyLCD <1763163252@qq.com>
2020-05-20 21:04:22 +0800

    文章列表查询筛选和删除文章业务功能第二遍完成

--- admin/js/article_list.js
+++ admin/js/article_list.js
@@ -1,6 +1,7 @@
 $(function () {
+    // 第一遍
     // 文章列表一打开就获取下拉框的option
-    $.ajax({
+    /* $.ajax({
         type: 'get',
         url: BigNew.category_list,
         success: function (res) {
@@ -79,6 +80,104 @@ $(function () {
     var articleId;
     $('#delModal').on('show.bs.modal', function (e) {
         console.log(e.relatedTarget);
+        articleId = $(e.relatedTarget).data('id');
+    })
+    $('.delModal .btn-sure').on('click', function () {
+        $.ajax({
+            type: 'post',
+            url: BigNew.article_delete,
+            data: {
+                id: articleId
+            },
+            success: function (res) {
+                // console.log(res);
+                if (res.code == 204) {
+                    $('.modal').modal('hide');
+                    getArticleDataList(currentPage, null);
+                }
+            }
+        })
+    }) */
+
+    // 第二遍
+    // 页面一加载就发送请求获取所有分类的数据
+    $.ajax({
+        type: 'get',
+        url: BigNew.category_list,
+        success: function (res) {
+            // console.log(res);
+            var htmlStr = template('opt', res);
+            $('#selCategory').html(htmlStr);
+        }
+    })
+    // 文章列表查询
+    getArticleDataList(1, pagination);
+    function getArticleDataList(myPage, callback) {
+        $.ajax({
+            type: 'get',
+            url: BigNew.article_query,
+            data: {
+                key: $('input[name=key]').val(),
+                type: $('#selCategory').val(),
+                state: $('#selStatus').val(),
+                page: myPage,
+                perpage: 7
+            },
+            success: function (res) {
+                // console.log(res);
+                var htmlStr = template('articlelist', res.data);
+                $('tbody').html(htmlStr);
+
+                // 当数据返回时立即启动分页功能
+                // pagination(res);
+                if (res.data.totalPage == 0 && myPage == 1) {
+                    $('#pagination-demo').hide().next().show();
+                } else if (res.data.totalPage != 0 && callback != null) {
+                    $('#pagination-demo').show().next().hide();
+                    callback(res);
+                } else if (res.data.totalPage != 0 && res.data.data.length == 0) {
+                    currentPage -= 1;
+                    $('#pagination-demo').twbsPagination('changeTotalPages', res.data.totalPage, currentPage);
+                }
+            }
+        })
+    }
+
+    // 点击筛选
+    $('#btnSearch').on('click', function (e) {
+        // 阻止默认提交行为
+        e.preventDefault();
+        // 点击筛选时，重绘页码控件
+        getArticleDataList(1, function (res) {
+            // 参数一为总页码发生改变执行的事件
+            // 参数二位总页码
+            // 参数三是默认显示的当前页
+            $('#pagination-demo').twbsPagination('changeTotalPages', res.data.totalPage, 1);
+        })
+    })
+
+    // 实现分页功能
+    var currentPage = 1;
+    function pagination(res, visiblePages) {
+        $('#pagination-demo').twbsPagination({
+            totalPages: res.data.totalPage,  //总页数
+            visiblePages: visiblePages || 7,  //每页显示的条数
+            first: '首页',
+            last: '最后一页',
+            next: '下一页',
+            prev: '上一页',
+            initiateStartPageClick: false,  //插件初始化时在起始页面上点击 false
+            onPageClick: function (event, page) {
+                // console.log(page);
+                currentPage = page;
+                getArticleDataList(page, null);
+            }
+        });
+    }
+
+    // 删除文章列表中的文章
+    var articleId;
+    $('#delModal').on('show.bs.modal', function (e) {
         articleId = $(e.relatedTarget).data('id');
     })
     $('.delModal .btn-sure').on('click', function () {
